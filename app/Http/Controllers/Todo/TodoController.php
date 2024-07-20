a52408257dd9208408982b87c15a3a85eff976e4
suffy <suffy.yanuar@gmail.com>
2024-07-20 10:40:25 +0700

    todolist crud laravel 11

---
+++ app/Http/Controllers/Todo/TodoController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Todo;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $max_data = 2;
+
+        if (request('search')) {
+            $data = Todo::where('task', 'like', '%' . request('search') . '%')->orderBy('task', 'asc')->paginate($max_data)->withQueryString();
+        }else{
+            $data = Todo::orderBy('task', 'asc')->paginate($max_data);
+        }
+
+        // dd($data);
+        return view('todo.app', compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'task' => 'required|min:3|max:10',
+        ],[
+            'task.required' => 'Task harus diisi',
+            'task.min' => 'Task minimal 3 karakter',
+            'task.max' => 'Task maksimal 10 karakter',
+        ]);
+
+        $data = [
+            'task' => $request->input('task'),
+        ];
+
+        Todo::create($data);
+        return redirect()->route('todo')->with('success', 'Task baru ditambahkan');
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'task' => 'required|min:3|max:100',
+        ],[
+            'task.required' => 'Task harus diisi',
+            'task.min' => 'Task minimal 3 karakter',
+            'task.max' => 'Task maksimal 100 karakter',
+        ]);
+
+        $data = [
+            'task' => $request->input('task'),
+            'is_done' => $request->input('is_done'),
+        ];
+
+        Todo::where('id', $id)->update($data);
+        return redirect()->route('todo')->with('success', 'Task baru diupdate');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Todo::where('id', $id)->delete();
+        return redirect()->route('todo')->with('success', 'Task dihapus');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Halo\HaloController;
+use App\Http\Controllers\Todo\TodoController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/todo', [TodoController::class, 'index'])->name('todo');
+Route::post('/todo', [TodoController::class, 'store'])->name('todo.post');
+Route::put('/todo/{id}', [TodoController::class, 'update'])->name('todo.update');
+Route::delete('/todo/{id}', [TodoController::class, 'destroy'])->name('todo.delete');
